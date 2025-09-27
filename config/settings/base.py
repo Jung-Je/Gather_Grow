0596c59feeb2a39a765607d7f76938b2c69b999c
Jung <jjlee030415@gmail.com>
2025-09-27 22:03:17 +0900

    :wrench: 이메일 및 템플릿 설정 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -95,7 +95,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "apps" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
