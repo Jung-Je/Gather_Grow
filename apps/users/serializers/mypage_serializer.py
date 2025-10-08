8aee83e78d18a830039cdaf8d0c76b0f00725de0
Jung <jjlee030415@gmail.com>
2025-10-09 00:03:29 +0900

    :sparkles: 비밀번호 변경 시 확인 필드 추가 및 프로필 수정 제한

--- apps/users/serializers/mypage_serializer.py
+++ apps/users/serializers/mypage_serializer.py
@@ -22,6 +22,7 @@ class ProfileSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = [
             "id",
+            "username",
             "email",
             "role",
             "joined_type",
@@ -33,12 +34,19 @@ class ProfileSerializer(serializers.ModelSerializer):
 class PasswordChangeSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
+    confirm_new_password = serializers.CharField(required=True, write_only=True)
 
     def validate_old_password(self, value):
         user = self.context["request"].user
         if not user.check_password(value):
-            raise serializers.ValidationError("Old password is incorrect")
+            raise serializers.ValidationError("현재 비밀번호가 일치하지 않습니다.")
         return value
+
+    def validate(self, data):
+        """새 비밀번호와 확인 비밀번호 일치 검증"""
+        if data.get('new_password') != data.get('confirm_new_password'):
+            raise serializers.ValidationError("새 비밀번호가 일치하지 않습니다.")
+        return data
 
     def update(self, instance, validated_data):
         instance.set_password(validated_data["new_password"])
