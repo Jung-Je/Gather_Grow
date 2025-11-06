1b7300a8a87d16442d5228f3744140f1e33a1769
Jung <jjlee030415@gmail.com>
2025-11-06 21:09:10 +0900

    :wrench: WebSocket 지원을 위한 ASGI 설정 업데이트

--- config/asgi.py
+++ config/asgi.py
@@ -9,8 +9,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
-application = get_asgi_application()
+# Django ASGI application을 먼저 초기화
+django_asgi_app = get_asgi_application()
+
+# WebSocket 라우팅 import
+from apps.chat.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": URLRouter(websocket_urlpatterns),
+    }
+)
